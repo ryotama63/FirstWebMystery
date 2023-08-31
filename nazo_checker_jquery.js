0d7b09e810774abc9919e4dcc850ababe975500f
ryotama63 <133328021+ryotama63@users.noreply.github.com>
2023-08-31 16:07:45 +0900

    Update nazo_checker_jquery.js

--- nazo_checker_jquery.js
+++ nazo_checker_jquery.js
@@ -1,34 +1,44 @@
 function AnswerCheck(form){
-    try{
-        // 入力欄に入っている文字列を拾う
-        send_text = $(form).children("input[type=text]")[0].value;
-        // 入力欄が空のとき、送信せず、エラーメッセージを出す
-        if(send_text.length == 0){
-            $("#result").text("入力欄が空です");
-            return false;
-        }
-
-        $.ajax({
-            url: "answer/"+send_text,
-            type: "GET",
-            dataType: "json",
-            timeout: 3000,
-            cache: false,
-        })
-        .done(function(resp){
-            // answers/{send_text}が存在する
-            if(resp["type"] == "move"){  // 遷移
-                window.location.href = resp["value"];
-            }else if(resp["type"] == "alert"){  // アラート
-                alert(resp["value"])
-            }
-        })
-        .fail(function(){
-            // answers/{send_text}が存在しない
-            $("#result").text("「"+send_text+"」は不正解です");
-        })
-    } catch(error){
-        console.error(error);
+  try{
+    // 連打を防止するために、送信ボタンを無効化する
+    $(form).find('button[type=submit]').prop('disabled', true);
+    // 入力欄に入っている文字列を拾う
+    send_text = $(form).children("input[type=text]")[0].value;
+    // console.log("./answer/"+form.name+send_text+".txt");
+    // 入力欄が空のとき、送信せず、エラーメッセージを出す
+    if(send_text.length == 0){
+      // $(form).next(".result").text("入力欄が空です");
+      return false;
     }
 
+    $.ajax({
+      url: "./answer/"+form.name+send_text+".txt",
+      type: "GET",
+      dataType: "json",
+      timeout: 3000,
+    })
+    .done(function(resp){
+      // answers/{send_text}が存在する
+      $(form).next(".result").text('');
+      if(resp["type"] == "move"){  // 遷移
+        window.location.href = resp["value"];
+      }else if(resp["type"] == "alert"){  // アラート
+        alert(resp["value"]);
+      }else if(resp["type"] == "uftext"){  // フォーム下テキスト
+        $(form).next(".result").text(resp["value"]);
+      }else if(resp["type"] == "function"){  // 関数実行
+        tmpFn[resp["value"]](form);
+      }
+    })
+    .fail(function(){
+        // ./answer/{form.name}{send_text}.txtが存在しない
+        $(form).next(".result").text("「"+send_text+"」は不正解です。");
+    })
+    .always(function(){
+        // 送信が完了したら、送信ボタンを有効化する
+        $(form).find('button[type=submit]').prop('disabled', false);
+    });
+  } catch(error){
+    console.error(error);
+  }
 }
